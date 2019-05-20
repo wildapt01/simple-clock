49bc8112235fe5f5568118a438e752aa4ed6dd00
Patrick_B <pb5191@gmail.com>
2019-05-20 10:42:41 -0400

    added draw_clock and pen definition

--- clock.py
+++ clock.py
@@ -1,1 +1,28 @@
 # Simple analogue clock in Python 3
+
+import turtle
+
+wndw = turtle.Screen()
+wndw.bgcolor("black")
+wndw.setup(width=600, height=600)
+wndw.title("Analogue Clock")
+
+# Create the drawing pen
+pen = turtle.Turtle()
+pen.hideturtle()
+pen.speed(0)
+pen.pensize(3)
+
+
+def draw_clock(pen):
+    pen.up()
+    pen.goto(0, 210)
+    pen.setheading(180)
+    pen.color("green")
+    pen.pendown()
+    pen.circle(210)
+
+
+draw_clock(pen)
+
+wndw.mainloop()
